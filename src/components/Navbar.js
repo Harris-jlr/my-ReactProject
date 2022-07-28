6d0ab21487e7dcee01501fb699063f557428cd84
Harris-jlr <Judith.Harris@nttdata.com>
2022-07-28 17:06:15 -0500

    updated home link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ export default function Example() {
                 </div>
                 <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
                   {/* Current: "border-pink-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                  <Link to="/Home" className="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                  <Link to="/" className="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                     Home
                   </Link>
                   <Link to="/Dashboard" className="border-transparent text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
