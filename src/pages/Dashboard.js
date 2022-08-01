01ab03c996709871b6eef8a1682a2d3cc4fe5c41
Harris-jlr <Judith.Harris@nttdata.com>
2022-08-01 18:20:47 -0500

    lots of json data changes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,63 +1,15 @@
 import { Fragment } from 'react'
 import {
-  CheckIcon,
   PaperClipIcon,
-  ThumbUpIcon,
-  UserIcon,
 } from '@heroicons/react/solid'
 import { Link } from 'react-router-dom'
+import timeline from '../timeline.json'
 
 const attachments = [
   { name: 'resume_front_end_developer.pdf', href: '#' },
   { name: 'coverletter_front_end_developer.pdf', href: '#' },
 ]
-const eventTypes = {
-  applied: { icon: UserIcon, bgColorClass: 'bg-gray-400' },
-  advanced: { icon: ThumbUpIcon, bgColorClass: 'bg-blue-500' },
-  completed: { icon: CheckIcon, bgColorClass: 'bg-green-500' },
-}
-const timeline = [
-  {
-    id: 1,
-    type: eventTypes.applied,
-    content: 'Applied to',
-    target: 'Front End Developer',
-    date: 'Sep 20',
-    datetime: '2020-09-20',
-  },
-  {
-    id: 2,
-    type: eventTypes.advanced,
-    content: 'Advanced to phone screening by',
-    target: 'Bethany Blake',
-    date: 'Sep 22',
-    datetime: '2020-09-22',
-  },
-  {
-    id: 3,
-    type: eventTypes.completed,
-    content: 'Completed phone screening with',
-    target: 'Martha Gardner',
-    date: 'Sep 28',
-    datetime: '2020-09-28',
-  },
-  {
-    id: 4,
-    type: eventTypes.advanced,
-    content: 'Advanced to interview by',
-    target: 'Bethany Blake',
-    date: 'Sep 30',
-    datetime: '2020-09-30',
-  },
-  {
-    id: 5,
-    type: eventTypes.completed,
-    content: 'Completed interview with',
-    target: 'Katherine Snyder',
-    date: 'Oct 4',
-    datetime: '2020-10-04',
-  },
-]
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,18 +1,4 @@
-const people = [
-    {
-      name: 'Leslie Alexander',
-      role: 'Co-Founder / CEO',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      name: 'Judy Harris',
-      role: 'Associate Director',
-      imageUrl:
-        'https://avatars.githubusercontent.com/u/60107512?v=4&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    // More people...
-  ]
+import people from '../people.json';
   
   export default function Example() {
     return (
