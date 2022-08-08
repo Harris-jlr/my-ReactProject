8ec41b2bc363fb3eaf0173f7134461e36c04f2c8
Harris-jlr <Judith.Harris@nttdata.com>
2022-08-08 12:44:47 -0500

    updated design page

--- src/pages/Designs.js
+++ src/pages/Designs.js
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import userQuestions from './components/UserQuestions';
+
 
 
 export default function Example() {
@@ -9,7 +9,7 @@ export default function Example() {
       <div className="Design">
       <Navbar />
         <div className="Content ">
-              <userQuestions />
+            
         </div>
       <Footer />
       </div>
