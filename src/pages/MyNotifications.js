ce868631892272b11a5b05d948e858df62863963
jharris10252012\jharris <jharris@fleetpride.local.dev>
2022-09-10 16:21:11 -0500

    updated errors

--- src/pages/MyNotifications.js
+++ src/pages/MyNotifications.js
@@ -1,5 +1,5 @@
 import { Fragment, useState } from 'react'
-import { Dialog, Menu, Popover, Tab, Transition } from '@headlessui/react'
+import { Dialog, Menu, Popover, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import { Link } from 'react-router-dom'
@@ -31,6 +31,7 @@ export default function Example() {
   return (
     <div className="bg-gray-50 ">
       <div>
+        <div>testing page my notifications</div>
         {/* Mobile menu */}
         <Transition.Root show={mobileMenuOpen} as={Fragment}>
           <Dialog as="div" className="relative z-40 lg:hidden" onClose={setMobileMenuOpen}>
@@ -69,48 +70,7 @@ export default function Example() {
                   </div>
 
                   {/* Links */}
-                  <Tab.Group as="div" className="mt-2">
-                    <div className="border-b border-gray-200">
-                      <Tab.List className="-mb-px flex px-4 space-x-8">
-                        {navigation.categories.map((category) => (
-                          <Tab
-                            key={category.name}
-                            className={({ selected }) =>
-                              classNames(
-                                selected ? 'text-indigo-600 border-indigo-600' : 'text-gray-900 border-transparent',
-                                'flex-1 whitespace-nowrap py-4 px-1 border-b-2 text-base font-medium'
-                              )
-                            }
-                          >
-                            {category.name}
-                          </Tab>
-                        ))}
-                      </Tab.List>
-                    </div>
-                    <Tab.Panels as={Fragment}>
-                      {navigation.categories.map((category) => (
-                        <Tab.Panel key={category.name} className="px-4 py-6 space-y-12">
-                          <div className="grid grid-cols-2 gap-x-4 gap-y-10">
-                            {category.featured.map((item) => (
-                              <div key={item.name} className="group relative">
-                                <div className="aspect-w-1 aspect-h-1 rounded-md bg-gray-100 overflow-hidden group-hover:opacity-75">
-                                  <img src={item.imageSrc} alt={item.imageAlt} className="object-center object-cover" />
-                                </div>
-                                <Link to={item.href} className="mt-6 block text-sm font-medium text-gray-900">
-                                  <span className="absolute z-10 inset-0" aria-hidden="true" />
-                                  {item.name}
-                                </Link>
-                                <p aria-hidden="true" className="mt-1 text-sm text-gray-500">
-                                  Shop now
-                                </p>
-                              </div>
-                            ))}
-                          </div>
-                        </Tab.Panel>
-                      ))}
-                    </Tab.Panels>
-                  </Tab.Group>
-
+                 
                   <div className="border-t border-gray-200 py-6 px-4 space-y-6">
                     {navigation.pages.map((page) => (
                       <div key={page.name} className="flow-root">
