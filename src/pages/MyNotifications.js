8d0a81126604c7eb6a7392863b0575c73b491682
jharris10252012\jharris <jharris@fleetpride.local.dev>
2022-09-10 16:04:34 -0500

    updated MyNotifications

--- src/pages/MyNotifications.js
+++ src/pages/MyNotifications.js
@@ -170,49 +170,7 @@ export default function Example() {
                   </div>
 
                   {/* Filters */}
-                  <form className="mt-4">
-                    {filters.map((section) => (
-                      <Disclosure as="div" key={section.name} className="border-t border-gray-200 px-4 py-6">
-                        {({ open }) => (
-                          <>
-                            <h3 className="-mx-2 -my-3 flow-root">
-                              <Disclosure.Button className="px-2 py-3 bg-white w-full flex items-center justify-between text-sm text-gray-400">
-                                <span className="font-medium text-gray-900">{section.name}</span>
-                                <span className="ml-6 flex items-center">
-                                  <ChevronDownIcon
-                                    className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-5 w-5 transform')}
-                                    aria-hidden="true"
-                                  />
-                                </span>
-                              </Disclosure.Button>
-                            </h3>
-                            <Disclosure.Panel className="pt-6">
-                              <div className="space-y-6">
-                                {section.options.map((option, optionIdx) => (
-                                  <div key={option.value} className="flex items-center">
-                                    <input
-                                      id={`filter-mobile-${section.id}-${optionIdx}`}
-                                      name={`${section.id}[]`}
-                                      defaultValue={option.value}
-                                      type="checkbox"
-                                      defaultChecked={option.checked}
-                                      className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
-                                    />
-                                    <label
-                                      htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                                      className="ml-3 text-sm text-gray-500"
-                                    >
-                                      {option.label}
-                                    </label>
-                                  </div>
-                                ))}
-                              </div>
-                            </Disclosure.Panel>
-                          </>
-                        )}
-                      </Disclosure>
-                    ))}
-                  </form>
+                  
                 </Dialog.Panel>
               </Transition.Child>
             </div>
@@ -285,57 +243,7 @@ export default function Example() {
                 </button>
 
                 <Popover.Group className="hidden sm:flex sm:items-baseline sm:space-x-8">
-                  {filters.map((section, sectionIdx) => (
-                    <Popover as="div" key={section.name} id="menu" className="relative z-10 inline-block text-left">
-                      <div>
-                        <Popover.Button className="group inline-flex items-center justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                          <span>{section.name}</span>
-                          {sectionIdx === 0 ? (
-                            <span className="ml-1.5 rounded py-0.5 px-1.5 bg-gray-200 text-xs font-semibold text-gray-700 tabular-nums">
-                              1
-                            </span>
-                          ) : null}
-                          <ChevronDownIcon
-                            className="flex-shrink-0 -mr-1 ml-1 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                            aria-hidden="true"
-                          />
-                        </Popover.Button>
-                      </div>
-
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Popover.Panel className="origin-top-right absolute right-0 mt-2 bg-white rounded-md shadow-2xl p-4 ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <form className="space-y-4">
-                            {section.options.map((option, optionIdx) => (
-                              <div key={option.value} className="flex items-center">
-                                <input
-                                  id={`filter-${section.id}-${optionIdx}`}
-                                  name={`${section.id}[]`}
-                                  defaultValue={option.value}
-                                  defaultChecked={option.checked}
-                                  type="checkbox"
-                                  className="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
-                                />
-                                <label
-                                  htmlFor={`filter-${section.id}-${optionIdx}`}
-                                  className="ml-3 pr-6 text-sm font-medium text-gray-900 whitespace-nowrap"
-                                >
-                                  {option.label}
-                                </label>
-                              </div>
-                            ))}
-                          </form>
-                        </Popover.Panel>
-                      </Transition>
-                    </Popover>
-                  ))}
+                 test popovergroup
                 </Popover.Group>
               </div>
             </section>
@@ -346,24 +254,7 @@ export default function Example() {
                 Products
               </h2>
 
-              <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
-                {products1.map((product) => (
-                  <Link key={product.id} to={product.href} className="group">
-                    <div className="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden sm:aspect-w-2 sm:aspect-h-3">
-                      <img
-                        src={product.imageSrc}
-                        alt={product.imageAlt}
-                        className="w-full h-full object-center object-cover group-hover:opacity-75"
-                      />
-                    </div>
-                    <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                      <h3>{product.name}</h3>
-                      <p>{product.price}</p>
-                    </div>
-                    <p className="mt-1 text-sm italic text-gray-500">{product.description}</p>
-                  </Link>
-                ))}
-              </div>
+              
             </section>
 
             <section aria-labelledby="featured-heading" className="relative mt-16 rounded-lg overflow-hidden lg:h-96">
@@ -398,24 +289,7 @@ export default function Example() {
                 More products
               </h2>
 
-              <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
-                {products2.map((product) => (
-                  <Link key={product.id} to={product.href} className="group">
-                    <div className="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden sm:aspect-w-2 sm:aspect-h-3">
-                      <img
-                        src={product.imageSrc}
-                        alt={product.imageAlt}
-                        className="w-full h-full object-center object-cover group-hover:opacity-75"
-                      />
-                    </div>
-                    <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                      <h3>{product.name}</h3>
-                      <p>{product.price}</p>
-                    </div>
-                    <p className="mt-1 text-sm italic text-gray-500">{product.description}</p>
-                  </Link>
-                ))}
-              </div>
+              
             </section>
           </div>
         </main>
