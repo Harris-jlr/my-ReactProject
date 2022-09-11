d677e6282c80fa59c3a090f4fd7deff8b9c83eac
jharris10252012\jharris <jharris@fleetpride.local.dev>
2022-09-10 20:56:37 -0500

    tray changes

--- src/components/slideinpanel.js
+++ src/components/slideinpanel.js
@@ -34,7 +34,8 @@ export default function Example() {
               >
                 <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
                   <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
-                    <div className="px-4 sm:px-6">
+                    <NotificationTray />
+                    {/* <div className="px-4 sm:px-6">
                       <div className="flex items-start justify-between">
                         <Dialog.Title className="text-lg font-medium text-gray-900">Panel title</Dialog.Title>
                         <div className="ml-3 flex h-7 items-center">
@@ -50,11 +51,11 @@ export default function Example() {
                           </button>
                         </div>
                       </div>
-                    </div>
+                    </div> */}
                     <div className="relative mt-6 flex-1 px-4 sm:px-6">
                        {/* Replace with your content */}
                       <div className="absolute px-4 sm:px-6 inset-y-0 right-0"> 
-                        <NotificationTray />
+                        
                       {/* /End replace */}
                       </div>
                     </div>
